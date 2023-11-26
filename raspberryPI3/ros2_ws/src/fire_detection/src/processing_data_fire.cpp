91fe20ea0ed6c7e4e18772a924cf4c5c7851f795
JohannCourand <johann.courand@gmail.com>
2023-11-26 13:07:57 +0100

    Modify processing node by adding moving average, standard deviation, and threshold

--- raspberryPI3/ros2_ws/src/fire_detection/src/processing_data_fire.cpp
+++ raspberryPI3/ros2_ws/src/fire_detection/src/processing_data_fire.cpp
@@ -1,47 +1,98 @@
+#include <vector>
+#include <cmath> 
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/string.hpp"
 #include "interfaces/msg/fire_sensor.hpp"
 
+
 class ProcessingDataFireNode : public rclcpp::Node
 {
-public: 
-    ProcessingDataFireNode() : Node("processing_data_fire_node") 
+public:
+    ProcessingDataFireNode() : Node("processing_data_fire_node"), window_size_{100}, counter_{0}
     {
-        subscription_processing_data = this->create_subscription<interfaces::msg::FireSensor>("data_fire", 10, bind(&ProcessingDataFireNode::dataFireCallBack, this, std::placeholders::_1)); 
+        buffer_.resize(window_size_, 0);
+        subscription_processing_data = this->create_subscription<interfaces::msg::FireSensor>("data_fire", 10, std::bind(&ProcessingDataFireNode::dataFireCallBack, this, std::placeholders::_1)
+        );
     }
-private: 
+
+private:
+    int processSensorData(int sensorValue)
+    {
+        if (counter_ < window_size_) 
+        {
+            buffer_[counter_] = sensorValue;
+            counter_++;
+            return 0; // Not enough data for processing
+        } 
+        else 
+        {
+            // Update the buffer with the new sensor value
+            buffer_[counter_ % window_size_] = sensorValue;
+            counter_++;
+
+            // Calculate the moving average
+            double movingAverage = calculateMovingAverage();
+
+            // Calculate the standard deviation
+            double standardDeviation = calculateStandardDeviation();
+
+            // Set dynamic thresholds
+            double lowerThreshold = movingAverage - 2 * standardDeviation;
+
+            if (lowerThreshold < 100) 
+            {
+                return 1; // Fire detected
+            }
+            else 
+            {
+                return 0; // No fire detected
+            }
+        }
+    }
 
     void dataFireCallBack(const interfaces::msg::FireSensor & msg)
     {
-        ir_sensor1_value = msg.ir_sensor1;
-        ir_sensor2_value = msg.ir_sensor2;
-        ir_sensor3_value = msg.ir_sensor3;
-        ir_sensor4_value = msg.ir_sensor4;
-        smoke_sensor1_value = msg.smoke_sensor1;
-        smoke_sensor2_value = msg.smoke_sensor2; 
-
-        if(ir_sensor2_value == 1 && ir_sensor1_value <= 350)
-        {
-            RCLCPP_INFO(get_logger(), "A fire is detected!");
-        }
-        else
-        {
-            RCLCPP_INFO(get_logger(), "Everything is fine!");
-        }
-    }
+        int result1 = processSensorData(msg.ir_sensor1);
+        int result2 = msg.ir_sensor2; 
+        //int result3 = processSensorData(msg.ir_sensor3);
+        //int result4 = msg.ir_sensor4; 
     
-    int ir_sensor1_value;
-    int ir_sensor2_value;
-    int ir_sensor3_value;
-    int ir_sensor4_value;
-    int smoke_sensor1_value;
-    int smoke_sensor2_value;
+        if (result1 == 1 || result2 == 1) 
+        {
+            RCLCPP_INFO(this->get_logger(), "Fire detected!");
+        }
+    }   
 
+    double calculateMovingAverage() const
+    {
+        double sum = 0.0;
+        for (int value : buffer_) {
+            sum += value;
+        }
+        return sum / window_size_;
+    }
+
+    double calculateStandardDeviation() const
+    {
+        double sumSquaredDifferences = 0.0;
+        double average = calculateMovingAverage();
+
+        for (int value : buffer_) {
+            sumSquaredDifferences += std::pow(value - average, 2);
+        }
+
+        return std::sqrt(sumSquaredDifferences / window_size_);
+    }
+
+    size_t window_size_;
+    size_t counter_;
+    std::vector<int> buffer_;
     rclcpp::Subscription<interfaces::msg::FireSensor>::SharedPtr subscription_processing_data;
-}; 
+};
 
 int main(int argc, char *argv[])
 {
+
     rclcpp::init(argc, argv); 
 
     auto Node = std::make_shared<ProcessingDataFireNode>(); 
