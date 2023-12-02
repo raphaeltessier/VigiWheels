e24eaba46076c2ecd200a87542dd52e6366847e3
ayce4u <148435653+ayce4u@users.noreply.github.com>
2023-12-02 13:57:40 +0100

    Update controlCmd.cpp

--- raspberryPI3/ros2_ws/src/car_control/src/controlCmd.cpp
+++ raspberryPI3/ros2_ws/src/car_control/src/controlCmd.cpp
@@ -19,33 +19,67 @@ float float_constante_correcteur_moteurA_right = (KI_right * Te + 2* KP_right)/ 
 float float_constante_correcteur_moteurB_right = (KI_right * Te - 2* KP_right)/ 2;
 
 //Speed variables
-float speedErrorLeft ;
-float speedErrorRight ;
-float consigneRPM ;
+float speedError_Left ;
+float speedError_Right ;
+float consigne_RPM ;
 
 
-void calculateRPMAuto(float consigneMotor, uint8_t& leftRearPwmCmd, uint8_t& rightRearPwmCmd,
-            float currentRPM_L, float currentRPM_R, float& lastError_L, float& lastError_R, float& correctedValue_L, 
-            float& correctedValue_R){
+void calculateRPM_Left_Auto(float consigne_Motor, uint8_t& leftRear_PwmCmd,
+            float currentRPM_L, float& lastError_L, float& correctedValue_L)
+            {
 
     //Transformation consigne to RPM
-    consigneRPM = gainRPM * consigneMotor;
+    consigne_RPM = gainRPM * consigne_Motor;
     
     // Calcul of the error for gain Kp
-    speedErrorLeft = consigneRPM - currentRPM_L;
-    speedErrorRight = consigneRPM - currentRPM_R;
+    speedError_Left = consigne_RPM - currentRPM_L;
 
     //Calcul of corrected value 
-    correctedValue_L = correctedValue_L + float_constante_correcteur_moteurA_left * speedErrorLeft + float_constante_correcteur_moteurA_left * lastError_L;
+    correctedValue_L = correctedValue_L + float_constante_correcteur_moteurA_left * speedError_Left + float_constante_correcteur_moteurB_left * lastError_L;
+    
+
+    // Calcul of the command to be sent to each motor (right and left)
+
+    if (correctedValue_L >100)
+        correctedValue_L= 100;
+    else if (correctedValue_L < 0)
+        correctedValue_L = 0;
+    
+    
+    
+    if (correctedValue_L >= 0 && correctedValue_L < 50)
+     leftRear_PwmCmd = correctedValue_L+ 50;
+
+    if (correctedValue_L >50 && correctedValue_L <=100)
+     leftRear_PwmCmd = correctedValue_L - 50;
+   
+    //Update the PWM values of both motors after command
+    leftRear_PwmCmd = correctedValue_L+ 50;
+
+    leftRear_PwmCmd = correctedValue_L- 50;
+    
+
+    // Calcul of the error for gain Ki
+    lastError_L = speedError_Left;
+   
+
+}
+void calculateRPM_Right_Auto(float consigne_Motor, , uint8_t& rightRear_PwmCmd,
+                            float currentRPM_R, float& lastError_R, float& correctedValue_R)
+    {
+
+    //Transformation consigne to RPM
+    consigne_RPM = gainRPM * consigne_Motor;
+    
+    // Calcul of the error for gain Kp
+
+    speedError_Right = consigne_RPM - currentRPM_R;
+
+    //Calcul of corrected value 
     correctedValue_R = correctedValue_R + float_constante_correcteur_moteurA_right * speedErrorRight + float_constante_correcteur_moteurB_right* lastError_R;
 
     // Calcul of the command to be sent to each motor (right and left)
 
-    if (correctedValue_L > 50)
-        correctedValue_L= 50;
-    else if (correctedValue_L < -50)
-        correctedValue_L = -50;
-    
     
     if (correctedValue_R > 50)
         correctedValue_R = 50;
@@ -54,14 +88,17 @@ void calculateRPMAuto(float consigneMotor, uint8_t& leftRearPwmCmd, uint8_t& rig
 
 
     //Update the PWM values of both motors after command
-    leftRearPwmCmd = correctedValue_L+ 50;
-    rightRearPwmCmd = correctedValue_R+ 50;
+    if (correctedValue_R >= 0 && correctedValue_R < 50)
+    rightRear_PwmCmd = correctedValue_R+ 50;
+     if (correctedValue_R > 50 && correctedValue_R <= 100)
+    rightRear_PwmCmd = correctedValue_R- 50;
 
     // Calcul of the error for gain Ki
-    lastError_L = speedErrorLeft;
-    lastError_R = speedErrorRight;
+    
+    lastError_R = speedError_Right;
 
 }
+/*
 /*
 void calculateRPMManual(float requestedThrottle, bool reverse, uint8_t& leftRearPwmCmd, uint8_t& rightRearPwmCmd,
             float currentRPM_L, float currentRPM_R, float& sumIntegralLeft, float& sumIntegralRight){
