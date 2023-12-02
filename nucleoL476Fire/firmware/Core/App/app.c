22e9fbef1e23102898803a1f14e6e06576b8e721
JohannCourand <johann.courand@gmail.com>
2023-12-02 19:55:31 +0100

    Nulceo commented - improved code readability and added descriptive comments

--- nucleoL476Fire/firmware/Core/App/app.c
+++ nucleoL476Fire/firmware/Core/App/app.c
@@ -11,16 +11,17 @@ extern UART_HandleTypeDef huart2;
 extern ADC_HandleTypeDef hadc1;
 extern ADC_HandleTypeDef hadc2;
 
-#define START_OF_FRAME '#'
-#define END_OF_FRAME '\n'
-#define SEPARATOR '='
+#define START_OF_FRAME '#'   // Start of frame delimiter
+#define END_OF_FRAME '\n'    // End of frame delimiter
+#define SEPARATOR '='        // Separator between ID and Value in the frame
 
-uint16_t ir_sensor1; // analog port A0
-uint16_t ir_sensor2; // digital port D2
-uint16_t ir_sensor3; // analog port A1
-uint16_t ir_sensor4; // digital port D3
-uint16_t smoke_sensor1; // digital port D4
-uint16_t smoke_sensor2; // digital port D4
+// Sensor values
+uint16_t ir_sensor1;        // Analog port A0
+uint16_t ir_sensor2;        // Digital port D2
+uint16_t ir_sensor3;        // Analog port A1
+uint16_t ir_sensor4;        // Digital port D3
+uint16_t smoke_sensor1;     // Digital port D4
+uint16_t smoke_sensor2;     // Digital port D4
 
 osMutexId_t uartMutex;
 osThreadId_t senderThreadId;
@@ -28,13 +29,14 @@ osThreadId_t receiverThreadId;
 
 const osThreadAttr_t senderTask_attributes = {
     .name = "Sender",
-    .priority = (osPriority_t) osPriorityHigh,  // Priorité élevée
-    .stack_size = 128 * 4  // Taille de la pile
+    .priority = (osPriority_t)osPriorityHigh,  // High priority
+    .stack_size = 128 * 4                       // Stack size
 };
 
 #define SIZE_RX 10
 #define SIZE_TX 100
 
+// Function to read analog sensor values
 void reading_analog_sensor(ADC_HandleTypeDef *hadc, uint16_t *sensorValue)
 {
     HAL_ADC_Start(hadc);
@@ -43,11 +45,13 @@ void reading_analog_sensor(ADC_HandleTypeDef *hadc, uint16_t *sensorValue)
     HAL_ADC_Stop(hadc);
 }
 
+// Function to read digital sensor values
 void reading_digital_sensor(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, uint16_t *sensorValue)
 {
     *sensorValue = !HAL_GPIO_ReadPin(GPIOx, GPIO_Pin);
 }
 
+// Function to read all sensor values
 void reading_sensor_values()
 {
     reading_analog_sensor(&hadc1, &ir_sensor1);
@@ -58,9 +62,10 @@ void reading_sensor_values()
     reading_analog_sensor(&hadc1, &smoke_sensor2);
 }
 
+// Frame format: "#[ID]=[Value]|[ID]=[Value]|[ID]=[Value]\n"
 void sending_data_uart(void *argument)
 {
-	char tx[SIZE_TX + 1];
+    char tx[SIZE_TX + 1];
 
     while (1)
     {
@@ -68,7 +73,9 @@ void sending_data_uart(void *argument)
 
         reading_sensor_values();
 
-        sprintf(tx, "#IR_SENSOR1=%hu|IR_SENSOR2=%u|IR_SENSOR3=%hu|IR_SENSOR4=%u|SMOKE_SENSOR1=4095|SMOKE_SENSOR2=4095\r\n", ir_sensor1, ir_sensor2, ir_sensor3, ir_sensor4);
+        // Create and transmit the frame
+        sprintf(tx, "#IR_SENSOR1=%hu|IR_SENSOR2=%u|IR_SENSOR3=%hu|IR_SENSOR4=%u|SMOKE_SENSOR1=4095|SMOKE_SENSOR2=4095\n",
+                ir_sensor1, ir_sensor2, ir_sensor3, ir_sensor4);
 
         HAL_UART_Transmit(&huart2, (const uint8_t *)tx, strlen(tx), 100);
 
@@ -78,8 +85,7 @@ void sending_data_uart(void *argument)
     }
 }
 
-//Frame "#[ID]=[Value]\n"
-
+// Frame format: "#[ID]=[Value]\n"
 void receiving_data_uart()
 {
     char buf[1];
@@ -95,7 +101,7 @@ void receiving_data_uart()
             HAL_UART_Receive(&huart2, (uint8_t *)buf, 1, 100);
         } while (*buf != START_OF_FRAME);
 
-        // Receiving data until "END_OF_FRAME"
+        // Receive data until "END_OF_FRAME"
         int i = 0;
         do
         {
@@ -106,13 +112,16 @@ void receiving_data_uart()
 
         rx[i] = '\0';
 
-        char *id = strtok(rx, "=");  // ID
-        char *value = strtok(NULL, "=");  // Value
+        // Extract ID and Value from the received frame
+        char *id = strtok(rx, "=");    // ID
+        char *value = strtok(NULL, "="); // Value
 
+        // Process based on ID and Value
         if (id != NULL && value != NULL)
         {
-            if (strcmp(id, "Fire") == 0) //"strcmp" : Retourne 0 si les chaînes sont égales
+            if (strcmp(id, "Fire") == 0)
             {
+                // Handle Fire sensor value
                 if (atoi(value) == 1)
                 {
                     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
@@ -123,19 +132,21 @@ void receiving_data_uart()
                 }
                 else
                 {
-                	//Error
+                    // Handle error
                 }
             }
             else if (strcmp(id, "PWM") == 0)
             {
-                //Axel
+                // Handle PWM value
+                // Axel
             }
             else
             {
-            	//Error
+                // Handle other IDs or report error
             }
         }
 
         osMutexRelease(uartMutex);
     }
 }
+
