835c408d15efc723c171b04ec24d9ef6fd51fa83
raphaeltessier <tessier.raphael@laposte.net>
2023-12-13 15:13:04 +0100

    starting following code, not working yet, need to be link to jetson topic

--- raspberryPI3/ros2_ws/src/servo_cam/src/servo_cam_node.cpp
+++ raspberryPI3/ros2_ws/src/servo_cam/src/servo_cam_node.cpp
@@ -5,6 +5,7 @@
 
 #include "interfaces/msg/servo_cam_order.hpp"
 #include "interfaces/msg/cam_pos_order.hpp"
+//#include "interfaces/msg/?????????.hpp"
 
 #include "std_srvs/srv/empty.hpp"
 
@@ -29,6 +30,9 @@ public:
         //suscriber
         subscription_cam_pos_order_ = this->create_subscription<interfaces::msg::CamPosOrder>(
         "cam_pos_order", 10, std::bind(&servo_cam::camPosOrderCallback, this, _1));
+
+        //subscription_?????????_order_ = this->create_subscription<interfaces::msg::?????????>(
+        //"?????????", 10, std::bind(&servo_cam::??????Callback, this, _1));
 
 
         //periodic function
@@ -57,24 +61,17 @@ private:
         requested_angle = pos_cmd.cam_angle;
     }
 
-/*
-    //calculate a pwm [5 , 10] for the servo from an command angle
-    float calculate_pwm(int angle) {
-        float pwm = float(angle)/36.0 + 5;
 
-
-        //security
-        if (pwm >= 10) {
-            pwm = 10;
-        }
-        else if (pwm <= 0) {
-            pwm = 0;
-        }
-
-        return pwm;
-
+    /* Update image position for mode 2 from image pos order feedback [callback function]  :
+    *
+    * This function is called when a message is published on the "/???????????" topic
+    * 
+    */
+    void ImagePosCallback(const interfaces::msg::??????? & pos_image){
+        x1 = pos_image.x1;
+        x2 = pos_image.x2;
     }
-*/
+
 
  //periodic function, see servo_cam_node.h to set period -> 100ms
     // to update the angular positon of the camera to scan
@@ -94,6 +91,14 @@ private:
                 sens = -sens;
             }
             
+        }
+        else if (mode == 2){
+            float mean = (x1 + x2)/2;
+            float correction = mean*FOV/RESOLUTION;
+            command_angle += int(correction);
+            if (command_angle >= 180) {command_angle = 180;} //saturation
+            else if (command_angle <= 0) {command_angle = 0;} //saturation
+
         }
 
         else {
@@ -119,6 +124,11 @@ private:
         int command_angle = 90; //[0, 180]
         int sens = PAS_SCAN; //{-10; 10}
 
+        float x1;
+        float x2;
+
+        float facteur = RESOLUTION/FOV;
+
 
 
         
@@ -129,7 +139,8 @@ private:
     rclcpp::Publisher<interfaces::msg::ServoCamOrder>::SharedPtr publisher_servo_cam_order_;
 
     //Suscribers
-    rclcpp::Subscription<interfaces::msg::CamPosOrder>::SharedPtr subscription_cam_pos_order_; 
+    rclcpp::Subscription<interfaces::msg::CamPosOrder>::SharedPtr subscription_cam_pos_order_;
+    //rclcpp::Subscription<interfaces::msg::?????????>::SharedPtr subscription_?????????_order_;  
 
     //Timer
     rclcpp::TimerBase::SharedPtr timer_;
