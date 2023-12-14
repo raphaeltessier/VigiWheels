3f236766e12d8a65e063259867aa677d8bb5c930
raphaeltessier <tessier.raphael@laposte.net>
2023-12-14 10:54:26 +0100

    merge from integration to have msg from ia

--- jetsonNano/ros2_ws/src/build/geicar_start_jetson/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/moad/VigiWheels/jetsonNano/ros2_ws/src/install/rplidar_ros/share/rplidar_ros/package.sh"
-. "/home/moad/VigiWheels/jetsonNano/ros2_ws/src/install/usb_cam/share/usb_cam/package.sh"
