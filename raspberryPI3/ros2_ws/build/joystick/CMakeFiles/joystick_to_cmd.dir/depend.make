18f2ab044d65a1fac7e796f58ddec9a7fb8c5589
raphaeltessier <tessier.raphael@laposte.net>
2023-12-18 18:51:22 +0100

    Follow fonctionnel, fréquence d'inférence faible ur la jeton entrainant du délai dans le focus

--- raspberryPI3/ros2_ws/build/joystick/CMakeFiles/joystick_to_cmd.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for joystick_to_cmd.
-# This may be replaced when dependencies are built.
--- raspberryPI3/ros2_ws/build/joystick/ament_cmake_python/joystick/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='joystick',
-    version='0.0.0',
-    packages=find_packages(
-        include=('joystick', 'joystick.*')),
-)
