4c0da0c14fe4d27ed9e6456d38869fc4fdc23057
Moad <melhadda@etud.insa-toulouse.fr>
2024-01-01 17:12:49 +0100

    pressure level

--- jetsonNano/ros2_ws/install/geicar_start_jetson/share/geicar_start_jetson/launch/geicar.jetson.launch.py
+++ jetsonNano/ros2_ws/install/geicar_start_jetson/share/geicar_start_jetson/launch/geicar.jetson.launch.py
@@ -37,10 +37,16 @@ def generate_launch_description():
         executable="test_node",
         emulate_tty=True
     )
+    pressure_level_node = Node(
+        package="pressure_level",
+        executable="pressure_node",
+        emulate_tty=True
+    )
 
 
     ld.add_action(lidar_node)
     ld.add_action(camera_node)
     ld.add_action(system_check_ack_node)
     ld.add_action(manometer_detection_node)
+    ld.add_action(pressure_level_node)
     return ld
