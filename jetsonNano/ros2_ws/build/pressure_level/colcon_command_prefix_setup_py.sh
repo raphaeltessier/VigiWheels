635c6e7e0a3c6ad4659ac9e09f9fe44a12b3f10f
raphaeltessier <tessier.raphael@laposte.net>
2024-01-14 18:23:08 +0100

    remove build log and install to be able to merge

--- jetsonNano/ros2_ws/build/pressure_level/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/moad/VigiWheels/jetsonNano/ros2_ws/install/interfaces/share/interfaces/package.sh"
--- jetsonNano/ros2_ws/build/pressure_level/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/moad/VigiWheels/jetsonNano/ros2_ws/install/pressure_level'
