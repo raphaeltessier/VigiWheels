1e1e2d0abb71c68f51b847d060db55b49f76dfbf
raphaeltessier <tessier.raphael@laposte.net>
2024-01-14 13:48:05 +0100

    remove build install and log in rasperry in order to merge

--- raspberryPI3/build/imu_tools/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/insa/Desktop/VigiWheels2.0/raspberryPI3/install/imu_complementary_filter/share/imu_complementary_filter/package.sh"
-. "/home/insa/Desktop/VigiWheels2.0/raspberryPI3/install/imu_filter_madgwick/share/imu_filter_madgwick/package.sh"
-. "/home/insa/Desktop/VigiWheels2.0/raspberryPI3/install/rviz_imu_plugin/share/rviz_imu_plugin/package.sh"
--- raspberryPI3/build/joystick/CMakeFiles/joystick_to_cmd.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for joystick_to_cmd.
-# This may be replaced when dependencies are built.
